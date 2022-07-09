dbe30082ef18d0c1361d9e11052f14b7a99ffee9
Shivam <dghd>
2022-07-09 12:16:27 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index');
+});
+
+
+
+module.exports = router;
